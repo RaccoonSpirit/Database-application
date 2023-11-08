74bdba5eab26a8852ae4c74885d7c458c5bb4e16
RaccoonSpirit <pavelgen25@gmail.com>
2023-11-08 19:38:44 +0300

    asda

--- Main.py
+++ Main.py
@@ -23,15 +23,47 @@ class TableModel(QtCore.QAbstractTableModel):
     def columnCount(self, index):
         return len(self._data[0])
 
-class TextDialog(QDialog):
+class TextDialogSearch(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Диалоговое окно")
+        self.setWindowTitle("Поиск")
         self.label = QLabel("Введите ФИО военнослужащего или название вооружения")
         self.text_input = QLineEdit()
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.text_input)
+        self.setLayout(layout)
+        button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+        layout.addWidget(button_box)
+    def get_text(self):
+        return self.text_input.text()
+class TextDialogAdd(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Добавление данных")
+        self.label_fio = QLabel("Введите ФИО военнослужащего ")
+        self.text_input_fio = QLineEdit()
+        self.label_date = QLabel("Дату рождения")
+        self.text_input_date = QLineEdit()
+        self.label_branch_number = QLabel("Номер отделения")
+        self.text_input_branch_number = QLineEdit()
+        self.label_rank = QLabel("Звание")
+        self.text_input_rank = QLineEdit()
+        self.label_name_weapon = QLabel("Название вооружения")
+        self.text_input_name_weapon = QLineEdit()
+        layout = QVBoxLayout()
+        layout.addWidget(self.label_fio)
+        layout.addWidget(self.text_input_fio)
+        layout.addWidget(self.label_date)
+        layout.addWidget(self.text_input_date)
+        layout.addWidget(self.label_branch_number)
+        layout.addWidget(self.text_input_branch_number)
+        layout.addWidget(self.label_rank)
+        layout.addWidget(self.text_input_rank)
+        layout.addWidget(self.label_name_weapon)
+        layout.addWidget(self.text_input_name_weapon)
         self.setLayout(layout)
 
         button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
@@ -40,7 +72,7 @@ class TextDialog(QDialog):
         layout.addWidget(button_box)
 
     def get_text(self):
-        return self.text_input.text()
+        return [self.text_input_fio.text(),self.text_input_date.text(),self.text_input_branch_number.text(),self.text_input_rank.text(),self.text_input_name_weapon.text()]
         
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -58,19 +90,27 @@ class MainWindow(QMainWindow):
         output_table.addAction(table_weapon)
         output_table.addAction(servicemans_weapon)
         
-        add_record = QAction('Поиск по бд', self)
-        add_record.triggered.connect(self.data_entry)
+        search_record = QAction('Поиск по бд', self)
+        search_record.triggered.connect(self.data_search)
+        add_record = QAction('Добавить запись в бд', self)
+        add_record.triggered.connect(self.data_add)
         
+        fileMenu.addAction(search_record)
         fileMenu.addAction(add_record)
         fileMenu.addMenu(output_table)
         self.setGeometry(500, 300, 750, 550)
         self.setWindowTitle('Приложение для работы с бд')
     
-    def data_entry(self):
-        dialog = TextDialog(self)
+    def data_search(self):
+        dialog = TextDialogSearch(self)
         if dialog.exec() == QDialog.DialogCode.Accepted:
             text = dialog.get_text()
             self.output_table(db.search_serviceman(text))
+    def data_add(self):
+        dialog = TextDialogAdd(self)
+        if dialog.exec() == QDialog.DialogCode.Accepted:
+            text = dialog.get_text()
+            print(text)
             
     def output_table(self, data:list):
         '''Метод выводящий таблицу'''
